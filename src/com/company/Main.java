9c7ce72142b6b425847921c74c48f1e385480c6b
Alekos Zournatzis <alekosdaz@gmail.com>
2021-09-26 18:13:41 +0300

    added test print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -25,6 +25,8 @@ public class Main {
             }
             results.close();
 
+            System.out.println("test");
+
             statement.close();
             conn.close();
         } catch (SQLException e){
